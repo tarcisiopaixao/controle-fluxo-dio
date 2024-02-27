b6399d3d044c8868ddbc030283af8e019f236383
tarcisiopaixao <tarcisio.santostj@hotmail.com>
2024-02-26 21:55:10 -0300

    Controle de fluxo composto

---
+++ controlo-fluxo/src/ResultadoEscolar.java
@@ -0,0 +1,17 @@
+public class ResultadoEscolar {
+    public static void main(String[] args) {
+       
+        int  nota = 6;
+
+        if (nota >= 7){
+
+            System.out.printf("Você foi aprovado sua nota foi: ", nota);
+
+        else
+        
+            System.out.println("Você foi reprovado, estude mais na proxima avaliação, sua nota foi: " + nota);
+            
+        }
+    }
+
+}
