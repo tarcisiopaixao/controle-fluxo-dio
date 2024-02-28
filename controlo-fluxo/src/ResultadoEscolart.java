1c2dd4d4f5493c10c6c51d00f5de9aa511c7e9e3
tarcisiopaixao <tarcisio.santostj@hotmail.com>
2024-02-28 17:54:28 -0300

    Adição de mais condicionais

--- controlo-fluxo/src/ResultadoEscolart.java
+++ controlo-fluxo/src/ResultadoEscolart.java
@@ -1,10 +1,10 @@
 public class ResultadoEscolart {
     public static void main(String[] args) {
 
-		int  nota = 7;
+		int  nota = 4;
 
-        String resultado = nota>=7 ? "Você foi aprovado, sua nota foi: "+ nota : "Você foi reprovado, sua nota foi"+nota;
-
+        String resultado = nota>=7 ? "Você foi aprovado, sua nota foi: "+ nota : nota>=5 && nota<7 ?
+        "Você foi RECUPERAÇÃO, sua nota foi: "+nota : "REPROVADO "+ nota;
         System.out.println(resultado);
     }
 }
