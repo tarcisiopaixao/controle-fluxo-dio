6ce011186e77715e829cab4c66f73436a1ff8b1c
tarcisiopaixao <tarcisio.santostj@hotmail.com>
2024-02-28 17:44:23 -0300

    Controle de fluxo ternário

---
+++ controlo-fluxo/src/ResultadoEscolart.java
@@ -0,0 +1,10 @@
+public class ResultadoEscolart {
+    public static void main(String[] args) {
+
+		int  nota = 7;
+
+        String resultado = nota>=7 ? "Você foi aprovado, sua nota foi: "+ nota : "Você foi reprovado, sua nota foi"+nota;
+
+        System.out.println(resultado);
+    }
+}
