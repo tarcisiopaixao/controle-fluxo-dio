0bbceff5f851b29a9a22f0e5b08156f3baa7523a
tarcisiopaixao <tarcisio.santostj@hotmail.com>
2024-03-01 08:06:49 -0300

    Estrutura de repetição For

---
+++ controlo-fluxo/src/ExemploFor.java
@@ -0,0 +1,8 @@
+public class ExemploFor {
+    public static void main(String[] args) {
+        for (int carneirinho = 1; carneirinho <=20; carneirinho ++){
+            System.out.println("Contando carneirinhos "+carneirinho);
+        }
+        System.out.println("Joãozinho domiu! ZzzzzzzzZZzzzzzzzZZ");
+    }
+}
