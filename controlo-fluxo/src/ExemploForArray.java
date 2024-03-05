2979f30a81581b668bdc086ccfbbb3c63be07ba9
tarcisiopaixao <tarcisio.santostj@hotmail.com>
2024-03-05 14:16:12 -0300

    Estruturas de repetição for array

---
+++ controlo-fluxo/src/ExemploForArray.java
@@ -0,0 +1,10 @@
+public class ExemploForArray {
+    public static void main(String[] args) {
+        String alunos [] = {"TARCISIO","NARA","GILMAR","CHICO","JOAÕZINHO","ISMAINE","KELLY","ANA MARIA","LETICIA"};
+
+        for (int x= 0; x < alunos.length; x++){
+
+            System.out.println("O Índice do aluno x= " + x + " é " + alunos [x]);
+        }
+    }
+}
